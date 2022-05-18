6924f6dbe1a221f5d7d293c0c8796de110c7dbc7
Manzar-Hassan <98890831+Manzar-Hassan@users.noreply.github.com>
2022-05-18 19:00:45 +0530

    Add files via upload

---
+++ day-4-tasks/displayFlagAPI.js
@@ -0,0 +1,19 @@
+//url - https://restcountries.com/v2/all
+
+// const http = new XMLHttpRequest();
+// http.onreadystatechange = handleRequest;
+// http.open("GET", "https://restcountries.com/v3.1/all");
+// http.send();
+
+
+// this will display all the country's flag
+// function handleRequest() {
+//   if (this.readyState === 4) {
+//     if (this.status === 200) {
+//       const data = JSON.parse(this.responseText);
+//       for(let i=0; i<data.length; i++) {
+//         console.log(data[i].flag);
+//       }
+//     }
+//   }
+// }
