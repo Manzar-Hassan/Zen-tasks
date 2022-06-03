ed0941f8b79898a8453b2e0845e01c1288c3b61b
Manzar-Hassan <98890831+Manzar-Hassan@users.noreply.github.com>
2022-06-03 14:59:39 +0530

    Update displayOthersAPI.js

--- day-4-tasks/displayOthersAPI.js
+++ day-4-tasks/displayOthersAPI.js
@@ -8,17 +8,12 @@ http.send();
 
 // this will display all the countries name, region, sub region and population
 
-let countryName = data[i].name.common;
-let region = data[i].region;
-let subRegion = data[i].subregion;
-let population = data[i].population;
-
 function handleRequest() {
   if (this.readyState === 4) {
     if (this.status === 200) {
       const data = JSON.parse(this.responseText);
       for(let i=0; i<data.length; i++) {
-        console.log(countryName, region, subRegion, population);
+        console.log(data[i].name.common, data[i].region, data[i].subregion, data[i].population);
       }
     }
   }
