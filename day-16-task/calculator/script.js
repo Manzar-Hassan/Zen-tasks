1c32ed409919f7a5dc7de47fdac0bb86a20389b6
Manzar-Hassan <98890831+Manzar-Hassan@users.noreply.github.com>
2022-06-04 19:35:11 +0530

    Add files via upload

--- day-16-task/calculator/script.js
+++ day-16-task/calculator/script.js
@@ -1,47 +1,62 @@
 //All created elements
 const wrapper = document.createElement("div");
 const container = document.createElement("div");
-const displayArea = document.createElement("div");
+const displayArea = document.createElement("input");
 const btnDiv = document.createElement("div");
 const buttonGroup = document.createElement("div");
-const addButton = document.createElement("div");
-const clrButton = document.createElement("div");
-const backButton = document.createElement("div");
-const multiplyButton = document.createElement("div"); //7 8 9 /
-const sevenButton = document.createElement("div");
-const eightButton = document.createElement("div");
-const nineButton = document.createElement("div");
-const divideButton = document.createElement("div");
-const fourButton = document.createElement("div");
-const fiveButton = document.createElement("div");
-const sixButton = document.createElement("div");
-const minusButton = document.createElement("div");
-const oneButton = document.createElement("div");
-const twoButton = document.createElement("div");
-const threeButton = document.createElement("div");
-const dotButton = document.createElement("div");
-const zeroButton = document.createElement("div");
-const doubleZeroButton = document.createElement("div");
-const equalsButton = document.createElement("div");
+const addButton = document.createElement("button");
+const clrButton = document.createElement("button");
+const backButton = document.createElement("button");
+const multiplyButton = document.createElement("button"); //7 8 9 /
+const sevenButton = document.createElement("button");
+const eightButton = document.createElement("button");
+const nineButton = document.createElement("button");
+const divideButton = document.createElement("button");
+const fourButton = document.createElement("button");
+const fiveButton = document.createElement("button");
+const sixButton = document.createElement("button");
+const minusButton = document.createElement("button");
+const oneButton = document.createElement("button");
+const twoButton = document.createElement("button");
+const threeButton = document.createElement("button");
+const dotButton = document.createElement("button");
+const zeroButton = document.createElement("button");
+const modulusButton = document.createElement("button");
+const equalsButton = document.createElement("button");
 const space = document.createElement("div");
 const space1 = document.createElement("div");
 const space2 = document.createElement("div");
 const space3 = document.createElement("div");
 
+const title = document.createElement('h1');
+const p = document.createElement('p');
+
+
+title.setAttribute('id','title');
+title.setAttribute('class','txt-color');
+title.innerText = 'DOM Calculator'
+
+p.setAttribute('id','description');
+p.setAttribute('class',' txt-color');
+p.innerText = 'This calculator is solely made by DOM manipulation and CSS'
+
 //main wrapper container
 wrapper.setAttribute(
   "class",
-  "container-fluid container-sm bg-white d-flex align-items-center justify-content-center height"
+  "container-fluid container-sm bg-white d-flex flex-column align-items-center justify-content-center height"
 );
 
 //container which holds calculator
-container.setAttribute("class", "col-12 col-sm-6 text-center sh-effect");
+container.setAttribute("class", "col-12 col-sm-6 text-center sh-effect mt-5");
 
 //display area
 displayArea.setAttribute(
   "class",
-  "col-12 h-auto bg-light font-size txt-color  p-2 scroll"
+  "col-12 h-auto bg-light font-size txt-color  p-2 scroll no-border"
 );
+displayArea.setAttribute('id','result')
+displayArea.setAttribute('value','0')
+displayArea.setAttribute('disabled','')
 displayArea.innerText = "0";
 
 //div container for row
@@ -58,75 +73,100 @@ space3.setAttribute("class", "w-100");
 
 //all the buttons
 addButton.setAttribute("class", "col btn btn-dark fs-3 square rmv-border");
+addButton.setAttribute("id", "add");
 addButton.innerText = "+";
 
 clrButton.setAttribute("class", "col btn btn-dark fs-3 square rmv-border");
+clrButton.setAttribute("id", "clear");
 clrButton.innerText = "c";
 
 backButton.setAttribute("class", "col btn btn-dark fs-3 square rmv-border");
+backButton.setAttribute("id", "back");
 backButton.innerText = "←";
 
 multiplyButton.setAttribute("class", "col btn btn-dark fs-3 square rmv-border");
+multiplyButton.setAttribute("id", "multiply");
 multiplyButton.innerText = "x";
 
 // --
 sevenButton.setAttribute("class", "col btn btn-dark fs-3 square rmv-border");
+sevenButton.setAttribute("id", "7");
 sevenButton.innerText = "7";
 
 eightButton.setAttribute("class", "col btn btn-dark fs-3 square rmv-border");
+eightButton.setAttribute("id", "8");
 eightButton.innerText = "8";
 
 nineButton.setAttribute("class", "col btn btn-dark fs-3 square rmv-border");
+nineButton.setAttribute("id", "9");
 nineButton.innerText = "9";
 
 divideButton.setAttribute(
   "class",
   "col btn btn-dark fs-6 pt-3 square rmv-border"
 );
+divideButton.setAttribute(
+  "id", "divide"
+);
+
 divideButton.innerText = "/";
 
 //
 fourButton.setAttribute("class", "col btn btn-dark fs-3 square rmv-border");
+fourButton.setAttribute("id", "4");
 fourButton.innerText = "4";
 
 fiveButton.setAttribute("class", "col btn btn-dark fs-3 square rmv-border");
+fiveButton.setAttribute("id", "5");
 fiveButton.innerText = "5";
 
 sixButton.setAttribute("class", "col btn btn-dark fs-3 square rmv-border");
+sixButton.setAttribute("id", "6");
 sixButton.innerText = "6";
 
 minusButton.setAttribute("class", "col btn btn-dark fs-3 square rmv-border");
+minusButton.setAttribute("id", "subtract");
 minusButton.innerText = "-";
 
 //
 oneButton.setAttribute("class", "col btn btn-dark fs-3 square rmv-border");
+oneButton.setAttribute("id", "1");
 oneButton.innerText = "1";
 
 twoButton.setAttribute("class", "col btn btn-dark fs-3 square rmv-border");
+twoButton.setAttribute("id", "2");
 twoButton.innerText = "2";
 
 threeButton.setAttribute("class", "col btn btn-dark fs-3 square rmv-border");
+threeButton.setAttribute("id", "3");
 threeButton.innerText = "3";
 
 dotButton.setAttribute("class", "col btn btn-dark fs-3 square rmv-border");
+dotButton.setAttribute("id", "dot");
 dotButton.innerText = ".";
 
 //
 zeroButton.setAttribute("class", "col btn btn-dark fs-3 square rmv-border");
+zeroButton.setAttribute("id", "0");
 zeroButton.innerText = "0";
 
-doubleZeroButton.setAttribute(
+modulusButton.setAttribute(
   "class",
   "col btn btn-dark fs-3 square rmv-border"
 );
-doubleZeroButton.innerText = "00";
+
+modulusButton.setAttribute(
+  "id", "00"
+);
+modulusButton.innerText = "%";
 
 equalsButton.setAttribute("class", "col btn btn-dark fs-3 square rmv-border");
+equalsButton.setAttribute("id", "equal");
 equalsButton.innerText = "=";
 
 //final append
 document.body.append(wrapper);
-wrapper.appendChild(container);
+wrapper.append(title, p, container);
 container.append(displayArea, btnDiv);
 btnDiv.append(buttonGroup);
 buttonGroup.append(
@@ -151,150 +191,150 @@ buttonGroup.append(
   addButton,
   space3,
   zeroButton,
-  doubleZeroButton,
+  modulusButton,
   equalsButton
 );
 
 //display buttons and their functions..
 function displayOneHandler() {
-  if (displayArea.innerText === "0" && displayArea.innerText.length === 1) {
-    displayArea.innerText = "1";
+  if (displayArea.value === "0" && displayArea.value.length === 1) {
+    displayArea.value = "1";
   } else {
-    displayArea.innerText = displayArea.innerText + "1";
+    displayArea.value = displayArea.value + "1";
   }
 }
 
 function displayTwoHandler() {
-  if (displayArea.innerText === "0" && displayArea.innerText.length === 1) {
-    displayArea.innerText = "2";
+  if (displayArea.value === "0" && displayArea.value.length === 1) {
+    displayArea.value = "2";
   } else {
-    displayArea.innerText = displayArea.innerText + "2";
+    displayArea.value = displayArea.value + "2";
   }
 }
 
 function displayThreeHandler() {
-  if (displayArea.innerText === "0" && displayArea.innerText.length === 1) {
-    displayArea.innerText = "3";
+  if (displayArea.value === "0" && displayArea.value.length === 1) {
+    displayArea.value = "3";
   } else {
-    displayArea.innerText = displayArea.innerText + "3";
+    displayArea.value = displayArea.value + "3";
   }
 }
 
 function displayFourHandler() {
-  if (displayArea.innerText === "0" && displayArea.innerText.length === 1) {
-    displayArea.innerText = "4";
+  if (displayArea.value === "0" && displayArea.value.length === 1) {
+    displayArea.value = "4";
   } else {
-    displayArea.innerText = displayArea.innerText + "4";
+    displayArea.value = displayArea.value + "4";
   }
 }
 
 function displayFiveHandler() {
-  if (displayArea.innerText === "0" && displayArea.innerText.length === 1) {
-    displayArea.innerText = "5";
+  if (displayArea.value === "0" && displayArea.value.length === 1) {
+    displayArea.value = "5";
   } else {
-    displayArea.innerText = displayArea.innerText + "5";
+    displayArea.value = displayArea.value + "5";
   }
 }
 
 function displaySixHandler() {
-  if (displayArea.innerText === "0" && displayArea.innerText.length === 1) {
-    displayArea.innerText = "6";
+  if (displayArea.value === "0" && displayArea.value.length === 1) {
+    displayArea.value = "6";
   } else {
-    displayArea.innerText = displayArea.innerText + "6";
+    displayArea.value = displayArea.value + "6";
   }
 }
 
 function displaySevenHandler() {
-  if (displayArea.innerText === "0" && displayArea.innerText.length === 1) {
-    displayArea.innerText = "7";
+  if (displayArea.value === "0" && displayArea.value.length === 1) {
+    displayArea.value = "7";
   } else {
-    displayArea.innerText = displayArea.innerText + "7";
+    displayArea.value = displayArea.value + "7";
   }
 }
 
 function displayEightHandler() {
-  if (displayArea.innerText === "0" && displayArea.innerText.length === 1) {
-    displayArea.innerText = "8";
+  if (displayArea.value === "0" && displayArea.value.length === 1) {
+    displayArea.value = "8";
   } else {
-    displayArea.innerText = displayArea.innerText + "8";
+    displayArea.value = displayArea.value + "8";
   }
 }
 
 function displayNineHandler() {
-  if (displayArea.innerText === "0" && displayArea.innerText.length === 1) {
-    displayArea.innerText = "9";
+  if (displayArea.value === "0" && displayArea.value.length === 1) {
+    displayArea.value = "9";
   } else {
-    displayArea.innerText = displayArea.innerText + "9";
+    displayArea.value = displayArea.value + "9";
   }
 }
 
 function displayZeroHandler() {
-  if (displayArea.innerText === "0" && displayArea.innerText.length === 1) {
-    displayArea.innerText = "0";
+  if (displayArea.value === "0" && displayArea.value.length === 1) {
+    displayArea.value = "0";
   } else {
-    displayArea.innerText = displayArea.innerText + "0";
+    displayArea.value = displayArea.value + "0";
   }
 }
 
 function displayDoubleZeroHandler() {
-  if (displayArea.innerText === "0" && displayArea.innerText.length === 1) {
-    displayArea.innerText = "0";
+  if (displayArea.value === "0" && displayArea.value.length === 1) {
+    displayArea.value = "";
   } else {
-    displayArea.innerText = displayArea.innerText + "00";
+    displayArea.value = displayArea.value + "%";
   }
 }
 
 function clrScreenHandler() {
-  displayArea.innerText = "0";
+  displayArea.value = "0";
 }
 
 function backButtonHandler() {
-  if (displayArea.innerText.length === 1) {
-    displayArea.innerText = "0";
+  if (displayArea.value.length === 1) {
+    displayArea.value = "0";
   } else {
-    displayArea.innerText = displayArea.innerText.slice(0, -1);
+    displayArea.value = displayArea.value.slice(0, -1);
   }
 }
 
 function addHandler() {
-  if (displayArea.innerText === "0" && displayArea.innerText.length === 1) {
-    displayArea.innerText = "0";
+  if (displayArea.value === "0" && displayArea.value.length === 1) {
+    displayArea.value = "0";
   } else {
-    displayArea.innerText = displayArea.innerText + "+";
+    displayArea.value = displayArea.value + "+";
   }
 }
 
 function subtractHandler() {
-  if (displayArea.innerText === "0" && displayArea.innerText.length === 1) {
-    displayArea.innerText = "0";
+  if (displayArea.value === "0" && displayArea.value.length === 1) {
+    displayArea.value = "0";
   } else {
-    displayArea.innerText = displayArea.innerText + "-";
+    displayArea.value = displayArea.value + "-";
   }
 }
 
 function multiplyHandler() {
-  if (displayArea.innerText === "0" && displayArea.innerText.length === 1) {
-    displayArea.innerText = "0";
+  if (displayArea.value === "0" && displayArea.value.length === 1) {
+    displayArea.value = "0";
   } else {
-    displayArea.innerText = displayArea.innerText + "*";
+    displayArea.value = displayArea.value + "*";
   }
 }
 
 function divideHandler() {
-  if (displayArea.innerText === "0" && displayArea.innerText.length === 1) {
-    displayArea.innerText = "0";
+  if (displayArea.value === "0" && displayArea.value.length === 1) {
+    displayArea.value = "0";
   } else {
-    displayArea.innerText = displayArea.innerText + "/";
+    displayArea.value = displayArea.value + "/";
   }
 }
 
 function dotButtonHandler() {
-  let str = displayArea.innerText;
+  let str = displayArea.value;
 
   if (str.includes(".")) {
     return;
   } else {
-    displayArea.innerText = str + ".";
+    displayArea.value = str + ".";
   }
 }
 
@@ -308,7 +348,7 @@ sevenButton.addEventListener("click", displaySevenHandler);
 eightButton.addEventListener("click", displayEightHandler);
 nineButton.addEventListener("click", displayNineHandler);
 zeroButton.addEventListener("click", displayZeroHandler);
-doubleZeroButton.addEventListener("click", displayDoubleZeroHandler);
+modulusButton.addEventListener("click", displayDoubleZeroHandler);
 clrButton.addEventListener("click", clrScreenHandler);
 backButton.addEventListener("click", backButtonHandler);
 dotButton.addEventListener("click", dotButtonHandler);
@@ -319,14 +359,50 @@ divideButton.addEventListener("click", divideHandler);
 
 //operation buttons and their functions...
 function outputHandler() {
-  let res = eval(displayArea.innerText);
+  let res = eval(displayArea.value)
 
   if(Number.isInteger(res)) {
-    displayArea.innerText = res;
+    displayArea.value = res;
 
   } else {
-    displayArea.innerText = res.toFixed(4);
+    displayArea.value = res.toFixed(4);
   }
 }
 
 equalsButton.addEventListener("click", outputHandler);
+
+
+//onkey press events and verification
+let validKeys = {
+  '1': displayOneHandler,
+  '2': displayTwoHandler,
+  '3': displayThreeHandler,
+  '4': displayFourHandler,
+  '5': displayFiveHandler,
+  '6': displaySixHandler,
+  '7': displaySevenHandler,
+  '8': displayEightHandler,
+  '9': displayNineHandler,
+  '0': displayZeroHandler,
+  '+': addHandler,
+  '%':displayDoubleZeroHandler,
+  '-': subtractHandler,
+  '*': multiplyHandler,
+  '/': divideHandler,
+  '.': dotButtonHandler,
+  'Backspace': backButtonHandler,
+  'Delete':clrScreenHandler,
+  'Enter':outputHandler,
+}
+
+document.body.addEventListener('keypress', function(event) {
+  if(validKeys.hasOwnProperty(event.key)) {
+    validKeys[event.key]();
+    console.log(event.key)
+    console.log(event.code)
+
+  } else {
+    alert('Only Numbers are Allowed')
+    console.log(event.key)
+  }
+});
